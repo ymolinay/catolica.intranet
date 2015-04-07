1ca7f46d4acb311f6e0bd0d7e5153d65f5fa97fd
heyrex <yrvingmolina@outlook.com>
2015-04-07 03:11:13 -0500

    terminado de filtro para ingresar notas

--- controller/__docenteSeccionCurso.php
+++ controller/__docenteSeccionCurso.php
@@ -43,7 +43,71 @@ if ($action == "comboboxDocente") {
     $combo = array();
     $combo = $objDocenteSeccionCursoDAO->CompleteComboboxDocente($objDocenteSeccionCursoDAO->objDocenteSeccionCurso);
     foreach ($combo as $key => $val) {
-        $cbx[$key] = array("idDocenteSeccionCurso" => $idDocenteSeccionCurso, "idUsuario" => $val->idUsuario, "prsNombre" => $val->prsNombre, "prsApellidoPaterno" => $val->prsApellidoPaterno, "prsApellidoMaterno" => $val->prsApellidoMaterno, "prsDNI" => $val->prsDNI);
+        $cbx[$key] = array("idDocenteSeccionCurso" => $val->idDocenteSeccionCurso, "idUsuario" => $val->idUsuario, "prsNombre" => $val->prsNombre, "prsApellidoPaterno" => $val->prsApellidoPaterno, "prsApellidoMaterno" => $val->prsApellidoMaterno, "prsDNI" => $val->prsDNI);
+    }
+    echo json_encode($cbx);
+}
+
+if ($action == "comboboxCarrera") {
+    $idDocenteSeccionCurso = $_GET['_idDoc'];
+    $idUsuario = $_GET['_idUsu'];
+    //$objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdDocenteSeccionCurso($idDocenteSeccionCurso);
+    $objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdUsuario($idUsuario);
+    $cbx = false;
+    $combo = array();
+    $combo = $objDocenteSeccionCursoDAO->CompleteComboboxCarrera($objDocenteSeccionCursoDAO->objDocenteSeccionCurso);
+    foreach ($combo as $key => $val) {
+        $cbx[$key] = array("idCarrera" => $val->idCarrera, "carDescripcion" => $val->carDescripcion, "carPeriodos" => $val->carPeriodos);
+    }
+    echo json_encode($cbx);
+}
+
+if ($action == "comboboxCiclo") {
+    $idDocenteSeccionCurso = $_GET['_idDoc'];
+    $idCarrera = $_GET['_idCar'];
+    $idUsuario = $_GET['_idUsu'];
+    //$objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdDocenteSeccionCurso($idDocenteSeccionCurso);
+    $objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdUsuario($idUsuario);
+    $cbx = false;
+    $combo = array();
+    $combo = $objDocenteSeccionCursoDAO->CompleteComboboxCarreraCiclo($objDocenteSeccionCursoDAO->objDocenteSeccionCurso, $idCarrera);
+    foreach ($combo as $key => $val) {
+        $cbx[$key] = array("idCiclo" => $val->idCiclo, "cloDescripcion" => $val->cloDescripcion);
+    }
+    echo json_encode($cbx);
+}
+
+if ($action == "comboboxCursos") {
+    $idDocenteSeccionCurso = $_GET['_idDoc'];
+    $idCarrera = $_GET['_idCar'];
+    $idUsuario = $_GET['_idUsu'];
+    $idCiclo = $_GET['_idCil'];
+    //$objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdDocenteSeccionCurso($idDocenteSeccionCurso);
+    $objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdUsuario($idUsuario);
+    $cbx = false;
+    $combo = array();
+    $combo = $objDocenteSeccionCursoDAO->CompleteComboboxCarreraCicloCurso($objDocenteSeccionCursoDAO->objDocenteSeccionCurso, $idCarrera, $idCiclo);
+    foreach ($combo as $key => $val) {
+        $cbx[$key] = array("idPlanEstudio" => $val->idPlanEstudio, "idCurso" => $val->idCurso, "crsNombre" => $val->crsNombre);
+    }
+    echo json_encode($cbx);
+}
+
+if ($action == "comboboxSecciones") {
+    $idDocenteSeccionCurso = $_GET['_idDoc'];
+    $idCarrera = $_GET['_idCar'];
+    $idUsuario = $_GET['_idUsu'];
+    $idCiclo = $_GET['_idCil'];
+    $idTurno = $_GET['_idTur'];
+    $idPlanEstudio = $_GET['_idPla'];
+    //$objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdDocenteSeccionCurso($idDocenteSeccionCurso);
+    $objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdUsuario($idUsuario);
+    $objDocenteSeccionCursoDAO->objDocenteSeccionCurso->setIdPlanEstudio($idPlanEstudio);
+    $cbx = false;
+    $combo = array();
+    $combo = $objDocenteSeccionCursoDAO->CompleteComboboxCarreraCicloCursoSeccion($objDocenteSeccionCursoDAO->objDocenteSeccionCurso, $idCarrera, $idCiclo, $idTurno);
+    foreach ($combo as $key => $val) {
+        $cbx[$key] = array("idSeccion" => $val->idSeccion, "scnDescripcion" => $val->scnDescripcion, "scnCantMaxima" => $val->scnCantMaxima, "scnInicio" => $val->scnInicio, "scnFin" => $val->scnFin);
     }
     echo json_encode($cbx);
 }
--- files/js/ingresarNotas.js
+++ files/js/ingresarNotas.js
@@ -1,5 +1,6 @@
-$(document).ready(function (){
-    comboboxDocente();
+$(document).ready(function () {
+    clearFormNotas();
+    comboboxTurno();
 });
 
 function comboboxDocente() {
@@ -8,6 +9,118 @@ function comboboxDocente() {
     var result = jqueryAjax(url, false, '');
     var docente = jQuery.parseJSON(result);
     for (i = 0; i < docente.length; i++) {
-        $('#idDocente').append(new Option(docente[i].prsNombre + ' ' + docente[i].prsApellidoPaterno + ' ' + docente[i].prsApellidoMaterno + ' - ' + docente[i].prsDNI, docente[i].idDocenteSeccionCurso));
+        var opt = new Option(docente[i].prsNombre + ' ' + docente[i].prsApellidoPaterno + ' ' + docente[i].prsApellidoMaterno + ' - ' + docente[i].prsDNI, docente[i].idDocenteSeccionCurso);
+        opt.setAttribute("data-usuario", docente[i].idUsuario);
+        $('#idDocente').append(opt);
     }
+}
+
+function comboboxTurno() {
+    $('#idTurno option[value!=""]').remove();
+    var url = baseHTTP + 'controller/__turno.php?action=combobox';
+    var result = jqueryAjax(url, false, '');
+    var jsonTurno = jQuery.parseJSON(result);
+    for (i = 0; i < jsonTurno.length; i++) {
+        $('#idTurno').append(new Option(jsonTurno[i].troDescripcion, jsonTurno[i].idTurno));
+    }
+}
+
+function comboboxCarreraNt() {
+    var docente = $('#idDocente');
+    $('#idCarrera option[value!=""]').remove();
+    if (docente.val() != '') {
+        var _idDocente = docente.val();
+        var _idUsuario = docente.find(':selected').data('usuario');
+        var url = baseHTTP + 'controller/__docenteSeccionCurso.php?action=comboboxCarrera&_idDoc=' + _idDocente + '&_idUsu=' + _idUsuario;
+        var result = jqueryAjax(url, false, '');
+        var docente = jQuery.parseJSON(result);
+        for (i = 0; i < docente.length; i++) {
+            $('#idCarrera').append(new Option(docente[i].carDescripcion, docente[i].idCarrera));
+        }
+    }
+}
+
+function comboboxCarreraCicloNt() {
+    var docente = $('#idDocente');
+    var carrera = $('#idCarrera');
+    $('#idCiclo option[value!=""]').remove();
+    if (docente.val() != '' && carrera.val() != '') {
+        var _idDocente = docente.val();
+        var _idCarrera = carrera.val();
+        var _idUsuario = docente.find(':selected').data('usuario');
+        var url = baseHTTP + 'controller/__docenteSeccionCurso.php?action=comboboxCiclo&_idDoc=' + _idDocente + '&_idCar=' + _idCarrera + '&_idUsu=' + _idUsuario;
+        var result = jqueryAjax(url, false, '');
+        var ciclo = jQuery.parseJSON(result);
+        for (i = 0; i < ciclo.length; i++) {
+            $('#idCiclo').append(new Option(ciclo[i].cloDescripcion, ciclo[i].idCiclo));
+        }
+    }
+}
+
+function comboboxCarreraCicloCursoNt() {
+    var docente = $('#idDocente');
+    var carrera = $('#idCarrera');
+    var ciclo = $('#idCiclo');
+    $('#idPlanEstudioCursos option[value!=""]').remove();
+    if (docente.val() != '' && carrera.val() != '' && ciclo.val() != '') {
+        var _idDocente = docente.val();
+        var _idCarrera = carrera.val();
+        var _idCiclo = ciclo.val();
+        var _idUsuario = docente.find(':selected').data('usuario');
+        var url = baseHTTP + 'controller/__docenteSeccionCurso.php?action=comboboxCursos&_idDoc=' + _idDocente + '&_idCar=' + _idCarrera + '&_idUsu=' + _idUsuario + '&_idCil=' + _idCiclo;
+        var result = jqueryAjax(url, false, '');
+        var planEstudio = jQuery.parseJSON(result);
+        for (i = 0; i < planEstudio.length; i++) {
+            $('#idPlanEstudioCursos').append(new Option(planEstudio[i].crsNombre, planEstudio[i].idPlanEstudio));
+        }
+    }
+}
+
+function comboboxCarreraCicloCursoSeccionNt() {
+    var docente = $('#idDocente');
+    var carrera = $('#idCarrera');
+    var ciclo = $('#idCiclo');
+    var planEstudioCurso = $('#idPlanEstudioCursos');
+    var turno = $('#idTurno');
+    $('#idSeccion option[value!=""]').remove();
+    $('#seccionFound').removeClass('label-success');
+    $('#seccionFound').addClass('label-default');
+    $('#seccionFound').html('No hay secciones.');
+    if (docente.val() != '' && carrera.val() != '' && ciclo.val() != '' && planEstudioCurso.val() != '' && turno.val() != '') {
+        var _idDocente = docente.val();
+        var _idCarrera = carrera.val();
+        var _idCiclo = ciclo.val();
+        var _idPlanEstudio = planEstudioCurso.val();
+        var _idTurno = turno.val();
+        var _idUsuario = docente.find(':selected').data('usuario');
+        var url = baseHTTP + 'controller/__docenteSeccionCurso.php?action=comboboxSecciones&_idDoc=' + _idDocente + '&_idCar=' + _idCarrera + '&_idUsu=' + _idUsuario + '&_idCil=' + _idCiclo + '&_idTur=' + _idTurno + '&_idPla=' + _idPlanEstudio;
+        var result = jqueryAjax(url, false, '');
+        var seccion = jQuery.parseJSON(result);
+
+        if (seccion == false) {
+            $('#seccionFound').removeClass('label-success');
+            $('#seccionFound').addClass('label-default');
+            $('#seccionFound').html('No hay secciones.');
+        } else {
+            $('#seccionFound').addClass('label-success');
+            $('#seccionFound').removeClass('label-default');
+            $('#seccionFound').html('Secciones encontradas.');
+        }
+
+        for (i = 0; i < seccion.length; i++) {
+            var opt = new Option(seccion[i].scnDescripcion, seccion[i].idSeccion);
+            opt.setAttribute("data-max", seccion[i].scnCantMaxima);
+            opt.setAttribute("data-ini", seccion[i].scnInicio);
+            opt.setAttribute("data-fin", seccion[i].scnFin);
+            $('#idSeccion').append(opt);
+        }
+    }
+}
+
+function clearFormNotas(){
+    comboboxDocente();
+    comboboxCarreraNt();
+    comboboxCarreraCicloNt();
+    comboboxCarreraCicloCursoNt();
+    comboboxCarreraCicloCursoSeccionNt();
 }
